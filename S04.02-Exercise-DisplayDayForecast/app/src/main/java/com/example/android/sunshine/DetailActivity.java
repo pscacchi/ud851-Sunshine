46661c05cca1cadc626b24c81f80ae5ab734b539
PScacchi <pabloantonio-scacchi@endava.com>
2022-01-19 14:13:37 -0300

    S04-01.DisplayDayForecast

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -13,5 +15,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         // TODO (2) Display the weather forecast that was passed from MainActivity
+        Intent intent = getIntent();
+        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String txt = intent.getStringExtra(Intent.EXTRA_TEXT);
+            Toast.makeText(this, txt, Toast.LENGTH_SHORT).show();
+        }
     }
 }
